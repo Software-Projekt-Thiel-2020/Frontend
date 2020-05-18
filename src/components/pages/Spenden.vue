8e6d197bb74e8e96038546d8f1ae343800853749
Joshua Martin <joshuamartin997googlemail.com>
2020-05-18 16:13:30 +0200

    Spendenseite wie im Mockup

--- src/components/pages/Spenden.vue
+++ src/components/pages/Spenden.vue
@@ -1,15 +1,84 @@
 <template>
+  <div>
     <v-container>
-        <h1>Spenden</h1>
+      <h1>Spenden</h1>
     </v-container>
+    <form class="container">
+      <div>
+        <label for="name">
+          <i class="fas fa-search"></i>
+        </label>
+        <input id="name" type="text" placeholder="Name des Betriebs" />
+      </div>
+      <div>
+        <label for="name">
+          <i class="fas fa-search"></i>
+        </label>
+        <input type="text" placeholder="Stadt/PLZ" />
+      </div>
+      <input class="submit" type="submit" value="Suchen" />
+    </form>
+    <v-container>
+        <h2>Ergebnisse:</h2>
+    </v-container>
+    <v-container class="results">
+        <div v-for="item in items" :key="item.name" class="project">
+            <img src="../../assets/placeholder.png">
+            <div style="border:0; margin-left: 0.5cm">
+            <h2>Firmenname: {{item.name}}</h2>
+            <h2>Straße: {{item.straße}} {{item.nummer}}</h2>
+            <h2>PLZ und Ort: {{item.plz}} {{item.ort}}</h2>
+            </div>
+        
+        </div>
+    </v-container>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Spenden"
-    }
+export default {
+  name: "Spenden",
+  data: () => ({
+        items: [
+            { name: "Edeka", straße: "Müllerstraße", nummer: 1, plz:32257, ort: "Bünde"},
+            { name: "ALDI", straße: "Müllerstraße", nummer: 1, plz:32257, ort: "Bünde"},
+            { name: "LIDL", straße: "Müllerstraße", nummer: 1, plz:32257, ort: "Bünde"},
+            { name: "REWE", straße: "Müllerstraße", nummer: 1, plz:32257, ort: "Bünde"},
+            { name: "Penny", straße: "Müllerstraße", nummer: 1, plz:32257, ort: "Bünde"},
+        ],
+        }),
+
+};
 </script>
 
-<style scoped>
 
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: row;
+}
+.container div {
+  border: 1px solid black;
+  margin-right: 1cm;
+  border-radius: 3px;
+}
+
+.submit {
+  background-color: #c4b3b0;
+  width: 100px;
+  font-weight: bold;
+  border-radius: 3px;
+  border: 1px solid black;
+}
+
+.results {
+    display: flex;
+    flex-direction: column;
+}
+
+.project {
+    display: flex;
+    flex-direction: row;
+    margin-bottom: 1cm;
+}
 </style>
