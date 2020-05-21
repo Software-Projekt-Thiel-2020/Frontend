f21638ce81b4eef74610ff285aa386779d6ad617
Maximilian Frömelt <maxifroemelt@gmail.com>
2020-05-21 19:13:02 +0200

    Added eslint config and dependencys for it

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  extends: [
+    'plugin:vue/recommended',
+    '@vue/airbnb',
+  ],
+  rules: {},
+  overrides: [
+    {
+      files: ['*.vue'],
+      rules: {
+        'max-len': 'off',
+      },
+    },
+  ],
+};
