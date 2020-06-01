481fbbfd8f5723b8d63842a87a9c0bf11350b623
Sascha <sascha.ruehrup@gmail.com>
2020-06-01 23:50:18 +0200

    Added content to Impressum.vue

--- src/components/pages/Impressum.vue
+++ src/components/pages/Impressum.vue
@@ -1,7 +1,59 @@
 <template>
-  <v-container>
-    <h1>Impressum</h1>
-  </v-container>
+  <div>
+    <v-container>
+      <h1>Impressum</h1>
+    </v-container>
+    <v-container>
+      <h3>Angaben gemäß § 5 TMG</h3>
+      <p>
+        Fh Bielefeld, Standort Minden<br>
+        Artilleriestraße, 9<br>
+        Gebäude D<br>
+        32427 Minden<br>
+      </p>
+      <h4>Vertreten durch:</h4>
+      <p>
+        Thomas Mustermann<br>
+        Alana Musterfrau
+      </p>
+      <h3>Kontakt</h3>
+      <p>
+        Telefon: +49(0) 123 44 55 66<br>
+        Telefax: +49(0) 123 44 55 99<br>
+        E-Mail: mustermann@fh-bielefeld.de
+      </p>
+      <h3>Umsatzsteuer-ID</h3>
+      <p>
+        Umsatzsteuer-Identifikationsnummer gemäß §27a Umsatzsteuergesetz:<br>
+        DE 999 999 999
+      </p>
+      <h3>EU-Streitschlichtung</h3>
+      <p>
+        Die Europäische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit:<br>
+        <a href="https://ec.europa.eu/consumers/odr">https://ec.europa.eu/consumers/odr.</a><br>
+        Unsere E-Mail-Adresse finden Sie oben im Impressum.
+      </p>
+      <h3>Verbraucherstreitbeilegung/Universalschlichtungsstelle</h3>
+      <p>
+        Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.
+      </p>
+      <h4>Haftung für Inhalte</h4>
+      <p>
+        Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen.<br>
+        Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.
+      </p>
+      <h4>Haftung für Links</h4>
+      <p>
+        Unser Angebot enthält Links zu externen Websiten Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunt der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar.<br><br>
+        Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.
+      </p>
+      <h3>Urheberrecht</h3>
+      <p>
+        Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet.<br><br>
+        Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.
+      </p>
+    </v-container>
+  </div>
 </template>
 
 <script>
