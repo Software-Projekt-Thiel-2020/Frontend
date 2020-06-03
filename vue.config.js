c048e711eadb32bb6b3870bfb834291b5845151a
Levent K <dermodmaster@googlemail.com>
2020-06-03 14:40:58 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ module.exports = {
     headers: {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-      'Access-Control-Allow-Headers': 'X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding'
+      'Access-Control-Allow-Headers': 'X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding',
     },
   },
 };
