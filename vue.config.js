a9dc4d172b3433ab035a769fe5421132ff8a7cfe
Levent K <dermodmaster@googlemail.com>
2020-06-09 22:00:20 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -8,6 +8,5 @@ module.exports = {
       'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
       'Access-Control-Allow-Headers': 'X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding',
     },
-    proxy: 'http://84.118.2.15:80/api/',
   },
 };
