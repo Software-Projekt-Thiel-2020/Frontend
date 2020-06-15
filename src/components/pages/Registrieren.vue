f1e4661a055749e920a821c8ac620693108f10e1
Joshua Martin <joshuamartin997googlemail.com>
2020-06-15 23:58:10 +0200

    Codelinting gefixed

--- src/components/pages/Registrieren.vue
+++ src/components/pages/Registrieren.vue
@@ -4,71 +4,71 @@
       <h1>Registrieren</h1>
     </v-container>
     <v-container>
-        <form class="container">
-            <v-row>
-                <v-col cols="9">
-                <v-text-field
-                v-model="vorname"
-                outlined
-                label="Vorname"
-                class="input"
-                />
-                </v-col>
-            </v-row>
-            <v-row>
-                <v-col cols="9">
-                <v-text-field
-                v-model="nachname"
-                outlined
-                label="Nachname"
-                class="input"
-                />
-                </v-col>
-            </v-row>
-            <v-row>
-                <v-col cols="9">
-                <v-text-field
-                v-model="username"
-                outlined
-                label="Benutzername"
-                class="input"
-                />
-                </v-col>
-            </v-row>
-            <v-row>
-                <v-col cols="9">
-                <v-text-field
-                v-model="pKey"
-                outlined
-                label="Public Key"
-                class="input"
-                />
-                </v-col>
-            </v-row>
-            <v-row>
-                <v-col cols="9">
-                <v-text-field
-                v-model="email"
-                outlined
-                label="E-Mail"
-                class="input"
-                />
-                </v-col>
-            </v-row>
-            <v-row>
-            <v-col cols="3">
+      <form class="container">
+        <v-row>
+          <v-col cols="9">
+            <v-text-field
+              v-model="vorname"
+              outlined
+              label="Vorname"
+              class="input"
+            />
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="9">
+            <v-text-field
+              v-model="nachname"
+              outlined
+              label="Nachname"
+              class="input"
+            />
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="9">
+            <v-text-field
+              v-model="username"
+              outlined
+              label="Benutzername"
+              class="input"
+            />
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="9">
+            <v-text-field
+              v-model="pKey"
+              outlined
+              label="Public Key"
+              class="input"
+            />
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="9">
+            <v-text-field
+              v-model="email"
+              outlined
+              label="E-Mail"
+              class="input"
+            />
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="3">
             <v-btn
-                class="submit"
-                type="submit"
-                block
-              >
-                Registrieren
-              </v-btn>
-            </v-col>
-            </v-row>
-        </form>
+              class="submit"
+              type="submit"
+              block
+            >
+              Registrieren
+            </v-btn>
+          </v-col>
+        </v-row>
+      </form>
     </v-container>
-    </div>
+  </div>
 </template>
 
 <script>
