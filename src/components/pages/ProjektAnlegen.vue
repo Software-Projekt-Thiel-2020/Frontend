cee9b6be8e8dab268953005c6cfefffe7dc8d08a
Sascha-Ruehrup <64094689+Sascha-Ruehrup@users.noreply.github.com>
2020-07-25 21:13:44 +0200

    Projekt anlegen erweitert um die optionalen Angaben (#92)
    * Institution loading, projektende frühestens aktueller Tag
    
    * weitere Meilensteine einfügen möglich. noch keine Begrenzungen vorhanden
    
    * weitere meilensteine nur im zeitraum vom gesamtprojekt, erst möglich nach eingabe von haupt endzeit

--- src/components/pages/ProjektAnlegen.vue
+++ src/components/pages/ProjektAnlegen.vue
@@ -30,8 +30,12 @@
     <v-row>
       <v-col cols="6">
         <v-overflow-btn
+          v-model="project.idInstitution"
           label="Institution (optional)"
-          target="#dropdown-example"
+          target="#dropdown-institution"
+          :items="allInstitutionsSortedNameId"
+          item-text="name"
+          item-value="id"
           auto-select-first
           outlined
           clearable
@@ -57,7 +61,7 @@
           type="number"
           min="1"
           oninput="validity.valid||(value='');"
-          label="Spendenziel in Szabo"
+          label="Spendenziel in Wei"
           outlined
           clearable
         />
@@ -81,8 +85,8 @@
           </template>
           <v-date-picker
             v-model="date"
-            min="2020-06-23"
-            @input="dateMenu = false"
+            :min="today"
+            @input="afterDayInput"
           />
         </v-menu>
       </v-col>
@@ -97,6 +101,7 @@
           <template v-slot:activator="{ on, attrs }">
             <v-text-field
               v-model="time"
+              :disabled="blockTime"
               label="Uhrzeit eingeben (GMT)"
               readonly
               v-bind="attrs"
@@ -106,9 +111,125 @@
           <v-time-picker
             v-model="time"
             format="24hr"
-            @input="timeMenu = false"
+            @input="afterTimeInput"
           />
         </v-menu>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12">
+        <v-data-table
+          :headers="headers"
+          :items="project.milestones"
+          sort-by="calories"
+        >
+          <template v-slot:top>
+            <v-toolbar
+              flat
+              color="white"
+            >
+              <v-toolbar-title>Weitere Meilensteine</v-toolbar-title>
+              <v-spacer />
+              <v-dialog
+                v-model="dialog2"
+                max-width="400"
+              >
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn
+                    :disabled="blockAdditionalMilestones"
+                    color="primary"
+                    dark
+                    v-bind="attrs"
+                    v-on="on"
+                  >
+                    Neues Item
+                  </v-btn>
+                </template>
+                <v-card>
+                  <v-card-title>
+                    <span class="headline">{{ formTitle }}</span>
+                  </v-card-title>
+                  <v-card-text>
+                    <v-container>
+                      <v-row>
+                        <v-col cols="12">
+                          <v-text-field
+                            v-model="editedItem.name"
+                            label="Meilensteinname"
+                            outlined
+                            clearable
+                            autofocus
+                          />
+                        </v-col>
+                        <v-col cols="12">
+                          <v-text-field
+                            v-model="editedItem.goal"
+                            label="Spendenziel in Wei"
+                            type="number"
+                            min="1"
+                            oninput="validity.valid||(value='');"
+                            outlined
+                            clearable
+                          />
+                        </v-col>
+                        <v-col cols="12">
+                          <v-text-field
+                            v-model="editedItem.requiredVotes"
+                            label="Benötigte Stimmen"
+                            type="number"
+                            min="1"
+                            oninput="validity.valid||(value='');"
+                            outlined
+                            clearable
+                          />
+                        </v-col>
+                        <v-col>
+                          <v-date-picker
+                            v-model="editedItem.until"
+                            :min="today"
+                            :max="date"
+                          />
+                        </v-col>
+                      </v-row>
+                    </v-container>
+                  </v-card-text>
+
+                  <v-card-actions>
+                    <v-spacer />
+                    <v-btn
+                      color="blue darken-1"
+                      text
+                      @click="close"
+                    >
+                      Cancel
+                    </v-btn>
+                    <v-btn
+                      color="blue darken-1"
+                      text
+                      @click="save"
+                    >
+                      Save
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
+            </v-toolbar>
+          </template>
+          <template v-slot:item.actions="{ item }">
+            <v-icon
+              small
+              @click="editItem(item)"
+            >
+              mdi-pencil
+            </v-icon>
+            <v-icon
+              small
+              @click="deleteItem(item)"
+            >
+              mdi-delete
+            </v-icon>
+          </template>
+        </v-data-table>
       </v-col>
     </v-row>
     <v-row>
@@ -153,11 +274,39 @@ import { userSession } from '../../userSession';
 export default {
   name: 'ProjektAnlegen',
   data: () => ({
+    blockTime: true,
+    blockAdditionalMilestones: true,
     dialog: {
       notLoggedIn: false,
       successful: false,
       error: false,
       errorMessage: '',
+    },
+    dialog2: false,
+    headers: [
+      {
+        text: 'Meilensteinname',
+        align: 'start',
+        sortable: false,
+        value: 'name',
+      },
+      { text: 'Spendenziel', value: 'goal' },
+      { text: 'benötigte Stimmen', value: 'requiredVotes' },
+      { text: 'Meilensteinende', value: 'until' },
+      { text: 'Actions', value: 'actions', sortable: false },
+    ],
+    editedIndex: -1,
+    editedItem: {
+      name: '',
+      goal: 1,
+      requiredVotes: 1,
+      until: null,
+    },
+    defaultItem: {
+      name: '',
+      goal: 1,
+      requiredVotes: 1,
+      until: null,
     },
     userSession: null,
     userData: null,
@@ -165,16 +314,31 @@ export default {
     dateMenu: false,
     timeMenu: false,
     date: '',
+    today: '',
     time: '',
+    allInstitutions: [],
+    allInstitutionsSortedNameId: [],
     project: {
       title: '',
       website: '',
-      idInstitution: 0,
+      idInstitution: null,
+      requiredVotes: null,
       goal: 1,
       until: 0,
       milestones: [],
     },
   }),
+  computed: {
+    formTitle() {
+      return this.editedIndex === -1 ? 'Neuer Meilenstein' : 'Meilenstein bearbeiten';
+    },
+    watch: {
+      dialog2(val) {
+        // eslint-disable-next-line no-unused-expressions
+        val || this.close();
+      },
+    },
+  },
   created() {
     this.userSession = userSession;
   },
@@ -184,18 +348,63 @@ export default {
     } else {
       this.dialog.notloggedIn = true;
     }
+    this.getTodaysDate();
+    this.getAllInstitutions();
   },
   methods: {
+    afterDayInput() {
+      this.dateMenu = false;
+      this.blockTime = false;
+    },
+    afterTimeInput() {
+      this.timeMenu = false;
+      this.blockAdditionalMilestones = false;
+      this.calcUntil();
+    },
+    editItem(item) {
+      this.editedIndex = this.project.milestones.indexOf(item);
+      this.editedItem = { ...item };
+      this.dialog2 = true;
+    },
+    deleteItem(item) {
+      const index = this.project.milestones.indexOf(item);
+      this.project.milestones.splice(index, 1);
+    },
+    close() {
+      this.dialog2 = false;
+      this.$nextTick(() => {
+        this.editedItem = { ...this.defaultItem };
+        this.editedIndex = -1;
+      });
+    },
+    save() {
+      if (this.editedIndex > -1) {
+        Object.assign(this.project.milestones[this.editedIndex], this.editedItem);
+      } else {
+        this.project.milestones.push(this.editedItem);
+      }
+      this.close();
+    },
+    getTodaysDate() {
+      this.today = new Date().toJSON().slice(0, 10);
+    },
+    getAllInstitutions() {
+      axios.get('/institutions').then((res) => {
+        this.allInstitutions = res.data;
+        for (let i = 0; i < this.allInstitutions.length; i += 1) {
+          this.allInstitutionsSortedNameId.push({ name: this.allInstitutions[i].name, id: this.allInstitutions[i].id });
+        }
+      });
+    },
     calcMainUntil() {
-      if (this.date !== '' && this.time !== '') {
-        this.calcUntil();
+      if (this.project.until !== 0) {
         this.createSpendenProjekt();
       }
     },
     calcUntil() {
       const dateArray = this.date.split(('-'), 3);
       const timeArray = this.time.split((':'), 2);
-      this.until = Date.UTC(parseInt(dateArray[0], 10), parseInt(dateArray[1], 10), parseInt(dateArray[2], 10),
+      this.project.until = Date.UTC(parseInt(dateArray[0], 10), parseInt(dateArray[1], 10), parseInt(dateArray[2], 10),
         parseInt(timeArray[0], 10), parseInt(timeArray[1], 10));
     },
     createSpendenProjekt() {
@@ -205,10 +414,11 @@ export default {
         website: this.project.website,
         idInstitution: this.project.idInstitution,
         goal: this.project.goal,
+        requiredVotes: this.project.requiredVotes,
         until: this.project.until,
         milestones: this.project.milestones,
       };
-      axios.post('projects', {}, { headers })
+      axios.post('/projects', {}, { headers })
         .then(() => {
           this.dialog.successful = true;
         })
