e70d3d8e460e492b84370cf94d5b5239c1ab641e
xGRKE <dmndnlrstz@gmail.com>
2020-08-02 18:45:38 +0200

    texte linksbuendig + WarumSpenden Bilder nur ab 700px

--- src/components/pages/Datenschutz.vue
+++ src/components/pages/Datenschutz.vue
@@ -5,27 +5,27 @@
     </v-container>
     <v-container>
       <h3>Verantwortlich im Sinne der Datenschutzgesetzes:  </h3>
-      <p>
+      <p class="text-left">
         ---------------------- <br>
         ----------------------<br>
         ----------------------
       </p>
       <h4>Datenschutz</h4>
-      <p>
+      <p class="text-left">
         Als Webseitenbetreiber nehmen wir den Schutz aller persönlichen Daten sehr ernst.
         Alle personenbezogenen Informationen werden vertraulich und gemäß den gesetzlichen Vorschriften behandelt,
         wie in dieser Datenschutzerklärung erläutert.
       </p>
-      <p>
+      <p class="text-left">
         Unserer Webseite kann selbstverständlich genutzt werden, ohne dass Sie persönliche Daten angeben. Wenn jedoch zu irgendeinem Zeitpunkt persönliche Daten wie z.B. Name, Adresse oder E-Mail abgefragt werden, wird dies auf freiwilliger Basis geschehen. Niemals werden von uns erhobene Daten ohne Ihre spezielle Genehmigung an Dritte weitergegeben.
         Datenübertragung im Internet, wie zum Beispiel über E-Mail, kann immer Sicherheitslücken aufweisen. Der komplette Schutz der Daten ist im Internet nicht möglich.
       </p>
       <h4>Auskunft, Löschung, Sperrung:</h4>
-      <p>
+      <p class="text-left">
         Zu jedem Zeitpunkt können Sie sich über die personenbezogenen Daten, deren Herkunft und Empfänger und den Nutzen der Datenverarbeitung informieren und unentgeltlich eine Korrektur, Sperrung oder Löschung dieser Daten verlangen. Bitte nutzen Sie dafür die im Impressum angegebenen Kontaktwege. Für weitere Fragen zum Thema stehen wir Ihnen ebenfalls jederzeit zur Verfügung.
         Server-Log-Files
       </p>
-      <p>
+      <p class="text-left">
         Der Seiten-Provider erhebt und speichert automatisch Daten in Server-Log Files, die von Ihrem Browser an uns übermittelt werden. Diese Daten enthalten:<br>
         <ul>
           <li>Browsertyp/ Browserversion</li>
@@ -35,24 +35,24 @@
           <li>Uhrzeit der Serveranfrage</li>
         </ul>
       </p>
-      <p>
+      <p class="text-left">
         Diese Daten sind nicht personenbezogen. Es erfolgt keine Zusammenführung dieser Daten mit anderen Datenquellen.
         Wenn uns konkrete Anhaltspunkte für eine rechtswidrige Nutzung bekannt werden behalten wir uns das Recht vor, diese Daten nachträglich zu überprüfen.
       </p>
       <h4>Cookies</h4>
-      <p>
+      <p class="text-left">
         Viele Internetseiten verwenden Cookies. Cookies sind unschädlich für Ihren Rechner und virenfrei.
         Sie dienen dazu, Internet-Angebote für die Besucher einer Webseite freundlicher,
         effektiver und sicherer zu machen. Cookies sind kleine Textdateien, die auf Ihrem Computer abgelegt werden
         und die Ihr Browser verwendet.
       </p>
-      <p>
+      <p class="text-left">
         Wir verwenden in der Regel so genannte „Session-Cookies“.
         Diese werden nach Verlassen unserer Seite automatisch gelöscht.
         Andere Cookies bleiben auf Ihrem Computer gespeichert, bis Sie diese löschen.
         Diese Cookies helfen dabei, Ihren Rechner beim nächsten Besuch wiederzuerkennen.
       </p>
-      <p>
+      <p class="text-left">
         Über die Browsereinstellungen können sie festlegen,
         dass Sie über neue Cookies informiert werden und Cookies jeweils annehmen müssen.
         Ebenso können Sie die Annahme von Cookies für bestimmte Fälle oder
@@ -60,14 +60,14 @@
         Werden Cookies desaktiviert, kann die Funktionalität unserer Website eingeschränkt sein.
       </p>
       <h4>Änderung der Datenschutzbestimmungen</h4>
-      <p>
+      <p class="text-left">
         Unsere Datenschutzerklärung können in unregelmäßigen Abständen angepasst werden,
         damit sie den aktuellen rechtlichen Anforderungen entsprechen oder um Änderungen unserer Dienstleistungen
         umzusetzen, z. B. bei der Einfügung neuer Angebote.
         Für Ihren nächsten Besuch gilt dann automatisch die neue Datenschutzerklärung.
       </p>
       <h4>Kontakt zum Datenschutzmitarbeiter</h4>
-      <p>
+      <p class="text-left">
         Für Fragen zum Datenschutz schicken Sie uns bitte eine Nachricht an mit dem Betreff „Datenschutz“.
         Diese Widerrufsbelehrung Seite wurde bei datenschutzerklärungmuster.de erstellt.
       </p>
