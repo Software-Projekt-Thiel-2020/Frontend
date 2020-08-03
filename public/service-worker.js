6db30823ccfcafca2d975fbd541ceecdd4467903
xGRKE <dmndnlrstz@gmail.com>
2020-08-03 19:41:56 +0200

    basic service worker done

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,11 @@
 
 const CACHE_NAME = 'SchlenderCache';
+const OFFLINE_NAME = 'offline.html';
 
 const FILES_TO_CACHE = [
-  ',',
+  '/favicon.png',
   '/offline.html',
+  '/manifest.json',
 ];
 
 // eslint-disable-next-line no-restricted-globals
@@ -40,35 +42,15 @@ self.addEventListener('activate', (evt) => {
 
 // eslint-disable-next-line no-restricted-globals
 self.addEventListener('fetch', (event) => {
+// CODELAB: Add fetch event handler here.
+  if (event.request.mode !== 'navigate') {
+    // Not a page navigation, bail.
+    return;
+  }
+
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-
-        return fetch(event.request).then(
-          (res) => {
-            // Check if we received a valid response
-            if (!res || res.status !== 200 || res.type !== 'basic') {
-              return res;
-            }
-
-            // IMPORTANT: Clone the response. A response is a stream
-            // and because we want the browser to consume the response
-            // as well as the cache consuming the response, we need
-            // to clone it so we have two streams.
-            const responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then((cache) => {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          },
-        );
-      }),
+    fetch(event.request)
+      .catch(() => caches.open(CACHE_NAME)
+        .then((cache) => cache.match(OFFLINE_NAME))),
   );
 });
