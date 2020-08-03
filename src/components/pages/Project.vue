0f7ffd1e4b568df0c11e6dde02d16a70b034ec8b
CodingJosh42 <joshuamartin997@googlemail.com>
2020-08-04 01:09:24 +0200

    Error Handling/ Bessere Benutzbarkeit (#114)
    * Gutscheine: Beim Laden Progress Circular,
    for-Schleife des Skeletonloaders auskommentiert, da es sonst agezeigt
    wird
    
    * Spenden: Progress Circular beim Laden der Institutionen
    
    * Spenden: Progress Circular bei filtern/ reseten der Ergebnisse
    
    * Gutscheine: Progress Circular beim filtern/reseten
    
    * Gutscheine: Standortbutton erhält loading Attribut
    
    * Spenden: Standortbutton loading Property
    
    * Spenden: Placeholder Bild wird richtig angezeigt
    
    * Gutscheine: Placeholderbild wird richtig angezeigt
    
    * Profil: Meine Institution Button unter Meine Spenden Button, nicht
    direkt daneben
    
    * Spendenseite: Loading Component wird jetzt beim Laden angezeigt
    
    * Spendenseite: Placeholder wird korrekt angezeigt
    
    * Gutscheinseite: Loading Component beim Laden des Projekts
    
    * Gutscheinseite: Bild der Institution wird angezeigt, Placeholder sollte
    auch richtig angezeigt werden. Konnte ich allerdings nicht überprüfen
    
    * Kommentare entfernt
    
    * Links zu den Websiten funktionieren nun
    
    * Progress Circular beim Laden der Profildaten
    
    * Profil: Änderungen Bestätigen Button Loading Component
    
    * Profil: Progress Circular zentraler
    
    * InstitutionEditieren: Loading Component hinzugefügt
    
    * Spendenseite: Error Snackbar falls uneingeloggt gespendet werden soll
    
    * Institution erstellen: Button Loading Component
    
    * Bild der Institution wird korrekt angezeigt
    
    * Bild der Institution wird beim Editieren richtig angezeigt
    
    * InstitutionEditieren: Bestätigen Button hat nun Loading Component,
    Overlay schließt sich erst wenn auch das Bild hochgeladen wurde
    
    * Protfolio: Loading Circle beim Laden der Spenden
    
    * Protfolio: Loading Circle beim Laden der Gutscheine
    
    * Protfolio: Einlösen-Button loading Component
    
    * Spendenseite: Error wird als Snackbar und nicht als v-alert dargestellt,
    da v-alert nicht verschwindet, wenn erfolgreich gespendet wurde
    
    * Portfolio: Wert der Gutscheine wird jetzt korrekt in Ether umgerechnet
    angezeigt
    
    * Spendenseite: Error Snackbar fix, Loading Component Button
    
    * Gutscheinseite: Gutscheine werden reloaded wenn ein Gutschein gekauft
    wurde
    
    * PorjekteEditieren: Loading Circle
    
    * Spendenseite: Loading Circle
    
    Co-authored-by: Joshua Martin <joshuamartin997googlemail.com>

--- src/components/pages/Project.vue
+++ src/components/pages/Project.vue
@@ -1,49 +1,5 @@
 <template>
   <div class="gradientBackground">
-    <div class="titleHeader text-center">
-      <h1
-        :class="$vuetify.breakpoint.smAndDown ? 'display-1' : 'display-3'"
-        class="font-weight-thin white--text"
-      >
-        {{ project.name }}
-      </h1>
-      <a
-        class=""
-        :href="project.homepage"
-      >
-        <v-btn
-          outlined
-          color="white"
-        >Webseite besuchen</v-btn>
-      </a>
-    </div>
-    <v-alert
-      v-if="errorMessage"
-      type="error"
-      tile
-    >
-      Fehler: {{ errorMessage }}
-    </v-alert>
-    <v-dialog
-      v-model="loading"
-      hide-overlay
-      persistent
-      width="300"
-    >
-      <v-card
-        color="primary"
-        dark
-      >
-        <v-card-text>
-          Anfrage wird bearbeitet...
-          <v-progress-linear
-            indeterminate
-            color="white"
-            class="mb-0"
-          />
-        </v-card-text>
-      </v-card>
-    </v-dialog>
     <v-dialog
       v-model="dialog"
       :max-width="$vuetify.breakpoint.smAndDown ? '95vw':'50vw'"
@@ -94,119 +50,173 @@
         </v-btn>
       </v-card>
     </v-dialog>
-    <v-container v-if="project">
-      <v-card
-        v-if="project"
-        elevation="7"
-        class="py-6 text-center projectBox"
+    <v-layout
+      v-if="loading == true"
+      justify-center
+    >
+      <v-progress-circular
+        :size="50"
+        :width="7"
+        color="green"
+        indeterminate
+        class="loadingCircle"
+      />
+    </v-layout>
+    <div v-else-if="project">
+      <div
+        class="titleHeader text-center"
       >
-        <v-card-text>
-          <v-row justify="center">
-            <v-col
-              cols="12"
-              style="max-width: 800px"
-            >
-              <v-card
-                v-if="project"
-                elevation="7"
-                class="text-center"
+        <h1
+          :class="$vuetify.breakpoint.smAndDown ? 'display-1' : 'display-3'"
+          class="font-weight-thin white--text"
+        >
+          {{ project.name }}
+        </h1>
+        <a
+          class=""
+          :href="'//'+project.webpage"
+        >
+          <v-btn
+            outlined
+            color="white"
+          >Webseite besuchen</v-btn>
+        </a>
+      </div>
+      <v-container>
+        <v-card
+          v-if="project"
+          elevation="7"
+          class="py-6 text-center projectBox"
+        >
+          <v-card-text>
+            <v-row justify="center">
+              <v-col
+                cols="12"
+                style="max-width: 800px"
               >
-                <v-system-bar
-                  color="secondary"
-                  height="40px"
+                <v-card
+                  v-if="project"
+                  elevation="7"
+                  class="text-center"
                 >
-                  <v-card-text
-                    class="headline font-weight-thin"
-                    style="color: white"
+                  <v-system-bar
+                    color="secondary"
+                    height="40px"
                   >
-                    Meilensteine
-                  </v-card-text>
-                </v-system-bar>
-                <div>
-                  <div
-                    v-for="milestone in project.milestones"
-                    :key="milestone.id"
-                  >
-                    <v-row>
-                      <v-col>
-                        <h4 class="title">
-                          Gesammelt
-                        </h4>
-                        <h1 class="title font-weight-light">
-                          {{ showValue(milestone.totalDonated) }}
-                        </h1>
-                      </v-col>
-                      <v-col>
-                        <h4 class="title">
-                          Ziel
-                        </h4>
-                        <h1 class="title font-weight-light">
-                          {{ showValue(milestone.goal) }}
-                        </h1>
-                      </v-col>
-                      <v-col>
-                        <h4 class="title">
-                          Votes
-                        </h4>
-                        <h1 class="title font-weight-light">
-                          {{ milestone.currentVotes }} von {{ milestone.requiredVotes }} Stimmen
-                        </h1>
-                      </v-col>
-                    </v-row>
-                    <h3>{{ (milestone.totalDonated/milestone.goal) > 1.0 ? 100 : Math.round((milestone.totalDonated/milestone.goal) * 100) }}%</h3>
-                    <v-progress-linear
-                      color="secondary"
-                      height="15"
-                      :value="(milestone.totalDonated/milestone.goal) * 100"
-                      striped
-                    />
+                    <v-card-text
+                      class="headline font-weight-thin"
+                      style="color: white"
+                    >
+                      Meilensteine
+                    </v-card-text>
+                  </v-system-bar>
+                  <div>
+                    <div
+                      v-for="milestone in project.milestones"
+                      :key="milestone.id"
+                    >
+                      <v-row>
+                        <v-col>
+                          <h4 class="title">
+                            Gesammelt
+                          </h4>
+                          <h1 class="title font-weight-light">
+                            {{ showValue(milestone.totalDonated) }}
+                          </h1>
+                        </v-col>
+                        <v-col>
+                          <h4 class="title">
+                            Ziel
+                          </h4>
+                          <h1 class="title font-weight-light">
+                            {{ showValue(milestone.goal) }}
+                          </h1>
+                        </v-col>
+                        <v-col>
+                          <h4 class="title">
+                            Votes
+                          </h4>
+                          <h1 class="title font-weight-light">
+                            {{ milestone.currentVotes }} von {{ milestone.requiredVotes }} Stimmen
+                          </h1>
+                        </v-col>
+                      </v-row>
+                      <h3>{{ (milestone.totalDonated/milestone.goal) > 1.0 ? 100 : Math.round((milestone.totalDonated/milestone.goal) * 100) }}%</h3>
+                      <v-progress-linear
+                        color="secondary"
+                        height="15"
+                        :value="(milestone.totalDonated/milestone.goal) * 100"
+                        striped
+                      />
+                    </div>
                   </div>
-                </div>
-              </v-card>
-            </v-col>
-            <v-col>
-              <v-card class="py-8">
-                <div>
-                  <img
-                    class="projectImage"
-                    :src="project.picturePath ? apiurl+'/file/'+project.picturePath : '../../assets/placeholder.png'"
-                  >
-                  <h4 class="headline font-weight-light">
-                    Jetzt Spenden!
-                  </h4>
-                  <currency-input
-                    v-model="donationValue"
-                    :allow-negative="false"
-                    :auto-decimal-mode="true"
-                    class="mt-3 headline"
-                    @change="compareInput"
-                  />
-                  <br>
-                  <h1 class="display-1">
-                    {{ getDonationETHValue ? getDonationETHValue : 0 }} ETH
-                  </h1>
-                  <v-checkbox
-                    v-model="voteEnabled"
-                    :disabled="voteDisabled"
-                    style="display:inline-flex"
-                    class="text-center align-center"
-                    label="Für Meilenstein abstimmen"
-                  />
-                  <br>
-                  <v-btn
-                    class="btn-hover color-9"
-                    dark
-                    @click="donate()"
-                  >
-                    Betrag Spenden
-                  </v-btn>
-                </div>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-card-text>
-      </v-card>
-    </v-container>
+                </v-card>
+              </v-col>
+              <v-col>
+                <v-card class="py-8">
+                  <div>
+                    <img
+                      v-if="project.picturePath"
+                      class="projectImage"
+                      :src="apiurl+'/file/'+project.picturePath"
+                    >
+                    <img
+                      v-else
+                      class="projectImage"
+                      src="../../assets/placeholder.png"
+                    >
+                    <h4 class="headline font-weight-light">
+                      Jetzt Spenden!
+                    </h4>
+                    <currency-input
+                      v-model="donationValue"
+                      :allow-negative="false"
+                      :auto-decimal-mode="true"
+                      class="mt-3 headline"
+                      @change="compareInput"
+                    />
+                    <br>
+                    <h1 class="display-1">
+                      {{ getDonationETHValue ? getDonationETHValue : 0 }} ETH
+                    </h1>
+                    <v-checkbox
+                      v-model="voteEnabled"
+                      :disabled="voteDisabled"
+                      style="display:inline-flex"
+                      class="text-center align-center"
+                      label="Für Meilenstein abstimmen"
+                    />
+                    <br>
+                    <v-btn
+                      class="btn-hover color-9"
+                      dark
+                      :loading="loadDonation"
+                      @click="donate()"
+                    >
+                      Betrag Spenden
+                    </v-btn>
+                  </div>
+                </v-card>
+              </v-col>
+            </v-row>
+          </v-card-text>
+        </v-card>
+      </v-container>
+    </div>
+    <v-snackbar
+      v-model="notLoggedin"
+      top
+      color="error"
+    >
+      Bitte melden Sie sich an
+    </v-snackbar>
+    <v-snackbar
+      v-model="error"
+      top
+      color="error"
+    >
+      Spende konnte nicht getätigt werden: {{ errorMessage }}
+    </v-snackbar>
   </div>
 </template>
 
@@ -225,10 +235,13 @@ export default {
     dialog: false,
     weiFormula: 1e18,
     errorMessage: null,
+    error: false,
     loading: false,
     voteEnabled: false,
     voteDisabled: true,
     apiurl: window.apiurl,
+    notLoggedin: false,
+    loadDonation: false,
   }),
   computed: {
     getDonationETHValue() {
@@ -252,26 +265,31 @@ export default {
   },
   methods: {
     donate() {
-      const donationAmount = this.getDonationETHValue * this.weiFormula;
-      const headers = {
-        authToken: this.userData.authResponseToken,
-        idproject: this.projectid,
-        amount: donationAmount,
-        voteEnabled: this.voteEnabled ? 1 : 0,
-      };
-      this.userData = window.userSession.loadUserData();
-      this.loading = true;
-      axios.post('donations', {}, { headers })
-        .then(() => {
-          this.openDialog();
-          this.loadProject();
-        })
-        .catch((err) => {
-          this.errorMessage = err.toString();
-        })
-        .finally(() => {
-          this.loading = false;
-        });
+      if (this.userData == null) {
+        this.notLoggedin = true;
+      } else {
+        const donationAmount = this.getDonationETHValue * this.weiFormula;
+        const headers = {
+          authToken: this.userData.authResponseToken,
+          idproject: this.projectid,
+          amount: donationAmount,
+          voteEnabled: this.voteEnabled ? 1 : 0,
+        };
+        this.userData = window.userSession.loadUserData();
+        this.loadDonation = true;
+        axios.post('donations', {}, { headers })
+          .then(() => {
+            this.openDialog();
+            this.loadProject();
+          })
+          .catch((err) => {
+            this.errorMessage = err.toString();
+            this.error = true;
+          })
+          .finally(() => {
+            this.loadDonation = false;
+          });
+      }
     },
     openDialog() {
       this.dialog = true;
@@ -439,5 +457,9 @@ export default {
     100% {
       box-shadow: inset 0px 0px 0px 30px #7ac142;
     }
+  }
+
+   .loadingCircle {
+    margin-top: 100px;
   }
 </style>
--- src/components/pages/ProjectGutschein.vue
+++ src/components/pages/ProjectGutschein.vue
@@ -73,7 +73,21 @@
             class="projectBox"
             elevation="4"
           >
-            <div class="d-flex flex-no-wrap justify-space-between">
+            <v-layout
+              v-if="loadingProject == true"
+              justify-center
+            >
+              <v-progress-circular
+                :size="50"
+                :width="7"
+                color="green"
+                indeterminate
+              />
+            </v-layout>
+            <div
+              v-else
+              class="d-flex flex-no-wrap justify-space-between"
+            >
               <div>
                 <v-card-title class="font-weight-light display-1">
                   Infos über den Betrieb
@@ -99,7 +113,14 @@
                 size="250"
                 tile
               >
-                <v-img :src="image" />
+                <img
+                  v-if="project[0].picturePath"
+                  :src="apiurl+'/file/'+project[0].picturePath"
+                >
+                <img
+                  v-else
+                  src="../../assets/placeholder.png"
+                >
               </v-avatar>
             </div>
           </v-card>
@@ -246,6 +267,7 @@ import { userSession } from '../../userSession';
 export default {
   name: 'ProjectGutschein',
   data: () => ({
+    apiurl: window.apiurl,
     userSession: null,
     userData: null,
     institutionId: null,
@@ -258,6 +280,7 @@ export default {
     image: 'https://i.imgur.com/EJOjIMC.jpg',
     vouchers: [],
     loadingVouchers: true,
+    loadingProject: true,
     dialogVoucher: {
       errorMessage: '',
       error: false,
@@ -311,12 +334,15 @@ export default {
         .catch((err) => {
           this.dialogProject.errorMessage = err.toString();
           this.dialogProject.error = true;
+        }).finally(() => {
+          this.loadingProject = false;
         });
     },
     loadVouchers() {
       let url = 'vouchers/institution?idInstitution=';
       url += this.institutionId;
       url += '&available=1';
+      this.loadingVouchers = true;
       axios.get(url)
         .then((res) => {
           this.vouchers = res.data;
@@ -373,6 +399,7 @@ export default {
     closeDialog() {
       this.dialog = false;
       this.$confetti.stop();
+      this.loadVouchers();
     },
   },
 };
