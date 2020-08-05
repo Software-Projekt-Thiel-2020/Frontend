f2df74838567de4368bd83b02f769981ad2154e0
Maximilian Frömelt <maxifroemelt@gmail.com>
2020-08-05 19:22:49 +0200

    Added MyDialog Component

---
+++ src/components/MyDialog.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-dialog
+    ref="myDialog"
+    :value="value"
+    :persistent="!persistent"
+    :fullscreen="$vuetify.breakpoint.xsOnly"
+    @input="update($event)"
+  >
+    <v-card>
+      <v-card-title class="headline grey lighten-2">
+        <slot name="title">
+          Header
+        </slot>
+      </v-card-title>
+      <v-card-subtitle>
+        <slot name="subtitle" />
+      </v-card-subtitle>
+
+      <v-card-text>
+        <slot name="text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </slot>
+      </v-card-text>
+
+      <v-divider v-if="this.$slots.actions" />
+
+      <v-card-actions>
+        <slot name="actions" />
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: 'MyDialog',
+  props: {
+    value: {
+      type: Boolean,
+      default: true,
+    },
+
+    persistent: {
+      type: Boolean,
+      default: true,
+    },
+
+  },
+  data: () => ({
+  }),
+  methods: {
+    update(v) {
+      this.$emit('input', v);
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
