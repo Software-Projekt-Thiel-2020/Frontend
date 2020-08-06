0e1d55274884e9a4d4a253f3217be5775c7e914c
Joshua Martin <joshuamartin997@googlemail.com>
2020-08-06 22:14:35 +0200

    Keine Meilensteine vorhanden wird angezeigt

--- src/components/pages/Project.vue
+++ src/components/pages/Project.vue
@@ -90,7 +90,6 @@
                   class="text-center"
                 >
                   <v-system-bar
-                    v-if="project.milestones.length > 0"
                     color="secondary"
                     height="40px"
                   >
@@ -101,6 +100,9 @@
                       Meilensteine
                     </v-card-text>
                   </v-system-bar>
+                  <v-card-text v-if="project.milestones.length == 0">
+                    Keine Meilensteine vorhanden
+                  </v-card-text>
                   <div>
                     <div
                       v-for="milestone in project.milestones"
