999113b3789b78a6e8a6f115d4c93082246f9649
Maximilian Frömelt <maxifroemelt@gmail.com>
2020-08-08 17:44:58 +0200

    Added loading prop to Default

--- src/components/Default.vue
+++ src/components/Default.vue
@@ -9,7 +9,19 @@
       </h1>
       <v-divider class="my-5" />
 
-      <slot />
+      <v-layout
+        v-if="loading"
+        justify-center
+      >
+        <v-progress-circular
+          :size="70"
+          :width="7"
+          color="green"
+          indeterminate
+          class="mt-24"
+        />
+      </v-layout>
+      <slot v-else />
     </v-container>
   </div>
 </template>
@@ -21,6 +33,10 @@ export default {
     title: {
       type: String,
       required: true,
+    },
+    loading: {
+      type: Boolean,
+      default: false,
     },
   },
 };
