8d4466f94886f213620d351a21d25a9999acd1e2
xGRKE <dmndnlrstz@gmail.com>
2020-08-13 21:09:05 +0200

    Revert "updated coords rule"
    This reverts commit 5060752551e17d53687ebb8215451392e28728c5.

--- src/components/pages/Institution.vue
+++ src/components/pages/Institution.vue
@@ -221,7 +221,6 @@ export default {
     ],
     coordRules: [
       (v) => !!v || 'Feld muss ausgefüllt werden',
-      (v) => (parseFloat(v) >= -90 && parseFloat(v) <= 90) || 'Bitte nur Werte im Bereich -90° bis 90° angeben',
       (v) => /^-?[0-9]*\.?[0-9]*$/s.test(v) || 'Bitte nur Zahlen eingeben',
     ],
     websiteRule: [
