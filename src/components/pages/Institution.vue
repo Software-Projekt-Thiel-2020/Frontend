633136818c1310789423fa241cb29f9f9fe4c68e
xGRKE <dmndnlrstz@gmail.com>
2020-08-13 22:27:38 +0200

    added missing rule

--- src/components/pages/Institution.vue
+++ src/components/pages/Institution.vue
@@ -226,6 +226,7 @@ export default {
     ],
     latRule: [
       (v) => !!v || 'Feld muss ausgefüllt werden',
+      (v) => (parseFloat(v) >= -90 && parseFloat(v) <= 90) || 'Bitte nur Werte im Bereich -90° bis 90° angeben',
       (v) => /^-?[0-9]*\.?[0-9]*$/s.test(v) || 'Bitte nur Zahlen eingeben',
     ],
     websiteRule: [
